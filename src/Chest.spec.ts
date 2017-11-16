47b9e81ca91db14fd960564a2a4cb068c4428438
Mike Pham <support@nativecode.com>
2017-11-15 22:59:27 -0500

    fixing test

--- src/Chest.spec.ts
+++ src/Chest.spec.ts
@@ -49,13 +49,13 @@ describe('when using RootProject to load a project', () => {
     })
   })
 
-  it('should run scripts for single project', (done) => {
+  it('should run scripts for single project', () => {
     const directory = Files.join(process.cwd(), 'testables', 'single')
     const args = Object.keys(Registry.all())
     return Chest.run(directory, ...args)
   })
 
-  it('should run scripts for workspace project', (done) => {
+  it('should run scripts for workspace project', () => {
     const directory = Files.join(process.cwd(), 'testables', 'workspaces')
     const args = Object.keys(Registry.all())
     return Chest.run(directory, ...args)
