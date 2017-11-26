431b29d83402d82b9319b09ff4ad794bc58cdfc8
Mike Pham <support@nativecode.com>
2017-11-26 15:51:43 -0500

    fixing tests

--- src/Core/Actions/Typings.spec.ts
+++ src/Core/Actions/Typings.spec.ts
@@ -1,13 +1,21 @@
-import 'mocha'
+import * as mocha from 'mocha'
 
 import { expect } from 'chai'
 import { Files, Registry } from '../index'
+
+const TIMEOUT = 10000
 
 const testables = Files.join(process.cwd(), 'testables')
 const single = Files.join(testables, 'single')
 const workspaces = Files.join(testables, 'workspaces')
 
 describe('when collecting type declarations', () => {
+
+  before(function (this: mocha.IHookCallbackContext) {
+    this.timeout(TIMEOUT)
+    const script = Registry.get('yarn')
+    return script.exec(single).then(() => script.exec(workspaces))
+  })
 
   it('should execute single', () => {
     const script = Registry.get('typings')
