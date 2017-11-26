f598a36944e1a010cd31509b69ebff36b9f2eb90
Mike Pham <support@nativecode.com>
2017-11-26 18:45:18 -0500

    fix(#0)

--- src/CLI.ts
+++ src/CLI.ts
@@ -1,3 +1,4 @@
 import { Chest } from './index'
 
-Chest.run(process.cwd(), ...process.argv.slice(2)).catch(() => process.exit(1))
+Chest.run(process.cwd(), ...process.argv.slice(2))
+  .catch(() => process.exit(1))
