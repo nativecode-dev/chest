b99840489ddea6851302172f1fe4d34ffb96c499
Mike Pham <support@nativecode.com>
2017-11-27 15:56:03 -0500

    fix: cleaning up output

--- src/Core/Actions/Typings.ts
+++ src/Core/Actions/Typings.ts
@@ -14,18 +14,14 @@ class Script extends UpdateScript {
   public exec(project: Project): Promise<Project> {
     return super.exec(project)
       .then(project =>
-        this.dependencies([project, ...project.children].map(proj => {
-          this.log.info('child', proj.name)
-          return proj
-        })).then(typings => this.typings(project, typings)).then(() => project)
+        this.dependencies([project, ...project.children])
+          .then(typings => this.typings(project, typings))
+          .then(() => project)
       )
   }
 
   private dependencies(projects: Project[]): Promise<string[]> {
-    return Promise.all(projects.map(project => {
-      this.log.task(project.name, project.path)
-      return project.npm
-    }))
+    return Promise.all(projects.map(project => project.npm))
       .then(npms => npms.map(npm => this.mergedeps(npm)))
       .then(deps => deps.reduce((previous, current) => previous.concat(current), []))
       .then(deps => Array.from(new Set(deps)).sort())
@@ -34,23 +30,12 @@ class Script extends UpdateScript {
   private mergedeps(npm: NPM): string[] {
     const deps = Object.keys(npm.dependencies || {})
     const devs = Object.keys(npm.devDependencies || {})
-    return Array.from(new Set([...deps, ...devs])).sort().map(dependency => {
-      this.log.info('mergedeps', npm.name, dependency)
-      return dependency
-    })
+    return Array.from(new Set([...deps, ...devs])).sort()
   }
 
   private typings(project: Project, typings: string[]): Promise<void> {
     return Promise.all(typings.map(typing => Files.join(project.path, 'node_modules', typing, 'package.json')))
-      .then(typings => {
-        typings.forEach(typing => this.log.info('typing', typing))
-        return typings
-      })
       .then(typings => Promise.all(typings.map(typing => Files.json<NPM>(typing))))
-      .then(npms => {
-        npms.forEach(npm => this.log.info('dependency', npm.name))
-        return npms
-      })
       .then(npms => npms.filter(npm => npm.types || npm.typings || npm.typeScriptVersion))
       .then(npms => {
         npms.forEach(npm => this.log.task(`${npm.name} ->`, npm.types || npm.typings || npm.name))
